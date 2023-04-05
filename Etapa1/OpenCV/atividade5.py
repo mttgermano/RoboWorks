d611c947800685e2260c53aef22a78c2baea2cbc
mttgermano <mttgermano@proton.me>
2023-04-05 04:33:22 +0000

    Update atividade5.py

--- Etapa1/OpenCV/atividade5.py
+++ Etapa1/OpenCV/atividade5.py
@@ -5,7 +5,7 @@ import cv2
 imagem = cv2.imread('ponte.jpg')
 
 for y in range(0, imagem.shape[0], 10): # percorre linhas
-    for x in range(0, imagem.shape[1], 10): # percorre colunas
+    for x in range(0, imagem.shape[1], 10): # percorre colunas, pulando de 10 em 10
         imagem[y:y+5, x: x+5] = (0,255,255) # cria um quadrado amarelo de 5x5 pixeis
 
 # Mostra a imagem com a função imshow
