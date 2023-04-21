481f06dac4fd285ac38fad17f724e64a1711838b
mttgermano <mttgermano@proton.me>
2023-04-21 02:10:04 +0000

    Update handTracking.py

--- Etapa2/HandTracking/handTracking.py
+++ Etapa2/HandTracking/handTracking.py
@@ -12,24 +12,24 @@ import time
 
 
  
-# Funcoes auxiliares ############################################
+# Funções auxiliares ############################################
 # conta os frames por segundo do video #
 def countFPS(t0: float,t1: float) -> tuple[int, float, float]:
-    t1 = time.time()        # obtem o tempo atual 
+    t1 = time.time()        # obtém o tempo atual 
 
-    fps = int(1//(t1-t0))   # divide-se 1 pela subtracao do tempo 
-                            # atual com o tempo passado, obtend o 
-                            # numero de frames 
+    fps = int(1//(t1-t0))   # divide-se 1 pela subtração do tempo 
+                            # atual com o tempo passado, obtendo 
+                            # o número de frames 
 
     t0 = t1                 # atualiza o tempo passado 
 
     return (fps, t0, t1)
 
-# soma o numero de dedos levantados #
+# soma o número de dedos levantados #
 def countDedos(pontos: list,dedos: list) -> int:
     numero_dedos = 0
     if pontos:
-        # para o dedao #
+        # para o dedão #
         # se o ponto superior do dedao estiver a direita do 
         # proximo ponto
         if pontos[4][0] < pontos[3][0]:
@@ -41,7 +41,7 @@ def countDedos(pontos: list,dedos: list) -> int:
             # se o ponto superior estiver abaixo de dois outros
             # pontos
             if pontos[x][1] < pontos[x-2][1]:
-                # numer de dedos abaixados e aumentado em 1
+                # número de dedos abaixados é aumentado em 1
                 numero_dedos += 1
     return numero_dedos
 
@@ -51,47 +51,47 @@ def countDedos(pontos: list,dedos: list) -> int:
 def main():
 
     # Variaveis Globais #########################################
-    t0,t1 = 0,0                 # variaveis de tempo inicial e 
-                                # final usadas para calcular fps
+    t0,t1 = 0,0                 # variáveis de tempo inicial e 
+                                # final usadas para calcular FPS
                                 # do video
                                     
     
     frame = cv.VideoCapture(0)  # cria um objeto que receberá o 
-                                # video obtido da primeira camera 
-                                # disponivel no computador 
+                                # vídeo obtido da primeira câmera 
+                                # disponível no computador 
                                 # (por isso o '0').
     
     color_text = (85,85,255)        # cor do texto 
     color_background = (40,50,42)   # cor do quadrado
     
-    # Shortcuts para metodos das bibliotecas ####################
+    # Shortcuts para métodos das bibliotecas ####################
     hands = mp.solutions.hands      # configura a biblioteca do
                                     # mediapiepe para usar 
-                                    # funcoes relacionadas a maos
+                                    # funções relacionadas a mãos
 
-    Hands = hands.Hands(max_num_hands=2)    # quantidade maxima 
-                                            # de maos que serao 
+    Hands = hands.Hands(max_num_hands=2)    # quantidade máxima 
+                                            # de mãos que serão 
                                             # detectadas
 
-    mpDwaw = mp.solutions.drawing_utils # configuracao da
+    mpDwaw = mp.solutions.drawing_utils # configuração da
                                         # biblioteca do mediapipe
-                                        # para funcoes
-                                        # relacionadas a desenhos
+                                        # para funções
+                                        # relacionadas à desenhos
     
 
     
     # Main Loop #################################################
     while(True):
-        ret,video = frame.read()    # captura cada frame do video
-        video = cv.flip(video,1)    # inverte o video
+        ret,video = frame.read()   # captura cada frame do vídeo
+        video = cv.flip(video,1)    # inverte o vídeo
     
     
-        # cria o retangulo de display que mostrara o numero de
+        # cria o retângulo de display que mostrara o número de
         # dedos levantados
         cv.rectangle(video,(20,20),(170,170),color_background,cv.FILLED)
     
     
-        # FPS section ###########################################
+        # FPS ###################################################
         fps,t0,t1 = countFPS(t0,t1)     # conta quantos frames 
                                         # por segundo o video 
                                         # esta conseguindo 
@@ -101,45 +101,42 @@ def main():
         cv.putText(video, f"FPS: {fps}",(200,50),cv.FONT_HERSHEY_PLAIN,2,color_text,3)
     
 
-        # Finger Numbers ########################################
+        # Número Dedos ##########################################
         results = Hands.process(video)  # faz o processamento do
                                         # video visando encontrar 
                                         # padroes similares a 
                                         # uma mao
 
-        # obtem as coordenadas dos pontos das maos
+        # obtém as coordenadas dos pontos das mãos
         handPoints = results.multi_hand_landmarks   
 
-        # extrai as dimensoes do video: sua altura e sua largura
+        # extrai as dimensões do vídeo: sua altura e sua largura
         altura, largura, _ = video.shape    
 
         # lista contendo os pontos
         pontos = []
     
-        # se existem coordenadas da mao, isto e, um objeto 
-        # parecido com uma mao foi encontrado no video
+        # se existem coordenadas da mão, isto é, se um objeto 
+        # parecido com uma mão foi encontrado no vídeo
         if handPoints:
 
-            # para cada ponto obtida a partir da mao
+            # para cada ponto obtida a partir da mão
             for points in handPoints:
 
-                # desenhe no video os pontos e suas conexoes
+                # desenhe no vídeo os pontos e suas conexões
                 mpDwaw.draw_landmarks(video, points,hands.HAND_CONNECTIONS)
 
-                # podemos enumerar esses pontos da seguinte 
-                # forma:
-                #
                 # para cada ponto, obtemos seu id e suas
                 # coordenadas (x,y)
                 for id, cord in enumerate(points.landmark):
 
-                    # obtem as coordenadas corretas dos pontos ao
-                    # se multiplicar pela dimensao da imagem
+                    # obtém as coordenadas corretas dos pontos ao
+                    # se multiplicar pela dimensão da imagem
                     cx, cy = int(cord.x * largura), int(cord.y * altura)
 
-                    # mostra no video o id dos pontos nas maos
-                    # (a implementacao da biblioteca da um id 
-                    # a certos pontos cruciais das maos)
+                    # mostra no vídeo o id dos pontos nas mãos
+                    # (a implementação da biblioteca enumera um 
+                    # id à certos pontos cruciais das mãos)
                     cv.putText(video,str(id),(cx,cy+10),cv.FONT_HERSHEY_PLAIN,1,color_text,2)
 
                     pontos.append((cx,cy))  # coloca as 
@@ -150,17 +147,17 @@ def main():
                 # lista com o id dos pontos superiores dos dedos
                 dedos = [8,12,16,20]
 
-                # conta o numero de dedos levantados
+                # conta o número de dedos levantados
                 numero_dedos = countDedos(pontos,dedos)
         
-                # printa o resultado do numero de dedos
-                # dentro do retangulo de display
+                # printa o resultado do número de dedos
+                # dentro do retângulo de display
                 cv.putText(video, f"{numero_dedos}",(48,153),cv.FONT_HERSHEY_PLAIN,10,color_text,3)
 
         #########################################################
     
     
-        # exibe os videos obtidos a partir da camera #
+        # exibe os vídeos obtidos à partir da camera #
         cv.imshow("video",video) 
     
         # waitKey retorna o código ascii do caractere pressionado   
@@ -169,6 +166,6 @@ def main():
     
     #############################################################
     
-    # Fecha o acesso a camera e tambem as janeas #
+    # Fecha o acesso à camera e tambem s janeas #
     frame.release()
     cv.destroyAllWindows()
